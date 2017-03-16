4b8b65ed5f19fe8256d3b9cf032616c57afa097e
junior <juniorjbn@gmail.com>
2017-03-16 17:16:24 -0300

    update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-oc -n getup new-app -f
-https://raw.githubusercontent.com/getupcloud/origin-build-monitor/master/origin-build-monitor.yaml
---param=CLIENTNAME=$1
+oc -n getup new-app -f origin-build-monitor.yaml --param=CLIENTNAME=$1
 
 oc -n getup policy add-role-to-user edit -z default
